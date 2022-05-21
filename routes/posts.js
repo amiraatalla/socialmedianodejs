50ef49a30afc89b8f44d9610165bf38f054820cf
amiraatalla <amiraatalla63@gmail.com>
2022-05-21 22:01:18 +0200

    update post

--- routes/posts.js
+++ routes/posts.js
@@ -3,7 +3,8 @@ const Post = require('../models/Post');
 const User = require('../models/User');
 
 
-router.post("/", async (req, res) => {
+//add post
+router.post("/new", async (req, res) => {
     const newPost = new Post(req.body);
     try {
         const savedPost = await newPost.save();
@@ -15,6 +16,29 @@ router.post("/", async (req, res) => {
         res.status(500).json(err);
     };
 });
+
+
+//update post
+router.put("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.userId === req.body.userId) {
+            await post.updateOne({
+                $set: req.body
+            });
+            res.status(200).json({
+                "message": "Post Updated successfully!!",
+                "data": post
+            });
+        }
+        else {
+            res.status(403).json("You can only update your posts !");
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    };
+});
+
 
 
 module.exports = router;
