1f4310672a892f688ded81744552e5ed83fbcbfb
amiraatalla <amiraatalla63@gmail.com>
2022-05-21 16:36:05 +0200

    connected to mongoose

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 
+//connect with mongoDB Database
+mongoose.connect(process.env.MONGO_DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("MongoDB is coonnected successfully");
+}).catch((err) => {
+    console.log(err);
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
