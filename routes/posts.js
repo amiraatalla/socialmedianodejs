182b02676dc16b4a8a51578663c25b53c5ee9e3e
amiraatalla <amiraatalla63@gmail.com>
2022-05-23 15:25:45 +0200

    like and dislike , get a post

--- routes/posts.js
+++ routes/posts.js
@@ -70,14 +70,33 @@ router.get("/getAllPosts", async (req, res) => {
     }
 });
 
-//post like API
-router.post("/:id/like",async(req, res)=>{
-    try{
+//post like/dislike API
+router.post("/:id/like", async (req, res) => {
+    try {
         const posts = await Post.findById(req.params.id);
-        if(!this.post.likes.includes(req.body.userId)){
-            await this.post.updateOne({$Push{likes:req.body.userId}});
+        if (!this.post.likes.includes(req.body.userId)) {
+            await this.post.updateOne({ $Push: { likes: req.body.userId } });
+            res.status(200).json("the post has been liked");
         }
-    }
+        else {
+            await this.post.updateOne({ $pull: { likes: req.body.userId } });
+            res.status(200).json("the post has been disliked");
+
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    };
 });
 
+
+
+//get a post
+router.get("/getAPost", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 module.exports = router;
