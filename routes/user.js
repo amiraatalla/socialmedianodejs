11498e0773143e405a0b592c641ce6cb0577509c
amiraatalla <amiraatalla63@gmail.com>
2022-06-04 23:21:56 +0200

    follow , un follow ,and friends lis

--- routes/user.js
+++ routes/user.js
@@ -25,12 +25,11 @@ router.get("/friends/:userId", async (req, res) => {
 router.put("/follow/:id", async (req, res) => {
     if (req.body.userId != req.params.id) {
         try {
-
             const user = await User.findById(req.params.id);
             const currentUser = await User.findById(req.body.userId);
-            if (!user.followers.includes(req.body.userId)) {
-                await User.updateOne({ $push: { followers: req.body.userId } }) &&
-                    await currentUser.updateOne({ $push: { $followings: req.params.id } });
+            if (!currentUser.followers.includes(req.body.userId)) {
+                await User.updateOne({ $push: { followers: req.body.userId } })
+                await currentUser.updateOne({ $push: { followings: req.params.id } });
                 res.status(200).json("User has been followed");
             }
             else {
@@ -50,9 +49,9 @@ router.put("/unfollow/:id", async (req, res) => {
         try {
             const user = await User.findById(req.params.id);
             const currentUser = await User.findById(req.body.userId);
-            if (!user.followers.includes(req.body.userId)) {
-                await User.updateOne({ $pull: { followers: req.body.userId } }) &&
-                    await currentUser.updateOne({ $pull: { $followings: req.params.id } });
+            if (currentUser.followers.includes(req.body.userId)) {
+                await User.updateOne({ $pull: { followers: req.body.userId } })
+                await currentUser.updateOne({ $pull: { followings: req.params.id } });
                 res.status(200).json("User has been unfollowed");
             }
             else {
